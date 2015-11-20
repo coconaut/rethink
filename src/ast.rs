f00b7bcc155759fe415ed498fe62d7cef1c66610
ambarry <ambarry22@gmail.com>
2015-11-20 00:50:04 -0500

    scaffolded ast

--- src/ast.rs
+++ src/ast.rs
@@ -1,13 +1,140 @@
 use proto::*;
 
-struct RQLQuery {
+// ----------------------------------------------------
+// Main RQL Query traits - semi polymorphic
+// ----------------------------------------------------
+pub trait RQLQuery {
+    // TODO: run fn
+    fn eq(&self) -> Term {Term {tt: TermType::Eq, st: "eq"}}
+
+}
+
+
+pub trait RQLBoolOperQuery : RQLQuery {
+
+}
+
+pub trait RQLBiOperQuery : RQLQuery {
+
+}
+
+pub trait RQLBiCompareOperQuery : RQLQuery {
+
+}
+
+pub trait RQLTopLevelQuery : RQLQuery {
+
+}
+
+pub trait RQLMethodQuery : RQLQuery {
+    // TODO: compose fn
+}
+
+pub trait RQLBracketQuery : RQLMethodQuery {
+
+}
+
+// ----------------------------------------------------
+// Semi specific structs
+// ----------------------------------------------------
+
+
+pub struct Datum {
+
+}
+impl Datum {
+
+}
+impl RQLQuery for Datum {}
+
+
+pub struct MakeArray {
+
+}
+impl MakeArray {
+
+}
+impl RQLQuery for MakeArray {}
+
+
+pub struct MakeObj {
+
+}
+impl MakeObj {
+
+}
+impl RQLQuery for MakeObj {}
+
+
+pub struct Var {
+    tt: TermType
+}
+impl Var {
+
+    //TODO: fn compose
+}
+
+// ----------------------------------------------------
+// Query structs. All should default impl their
+// respective query types, should enable the method chaining
+// without rewriting.
+// Doesn't seem worth a bunch of dup structs with same props,
+// so trying to keep it simpler with Terms and let the
+// query funcs assign the defaults, since we'd need to do it
+// anyway.
+// ----------------------------------------------------
+
+pub struct Term {
     tt: TermType,
     st: &'static str
 }
+impl RQLQuery for Term { }
 
-impl RQLQuery {
-    fn get(&self) -> RQLQuery {
-        let q = RQLQuery {tt: TermType::GET, st: "get"};
-        q
-    }
+
+pub struct TopLevelTerm {
+    tt: TermType,
+    st: &'static str
+}
+impl RQLTopLevelQuery for TopLevelTerm {}
+
+
+pub struct MethodTerm {
+    tt: TermType,
+    st: &'static str
+}
+impl RQLMethodQuery for MethodTerm {}
+
+
+pub struct BiCompareOperTerm {
+    tt: TermType,
+    st: &'static str
+}
+impl RQLBiCompareOperQuery for BiCompareOperTerm {}
+
+
+pub struct BiOperTerm {
+    tt: TermType,
+    st: &'static str
+}
+impl RQLBiOperQuery for BiOperTerm {}
+
+
+pub struct BracketTerm {
+    tt: TermType,
+    st: &'static str
+}
+impl RQLBracketQuery for BracketTerm {}
+
+
+// ----------------------------------------------------
+// Main db structs
+// ----------------------------------------------------
+
+pub struct Table {
+    fn get(&self) -> Term {tt: TermType::GET, st: "get"}
+}
+impl RQLQuery for Table {}
+
+pub struct DB {
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate rethink;
 use rethink::net;
-
+use rethink::ast::*;
 
 fn main() {
-    let stream = net::connect();
-    match stream {
-        Some(mut s) => net::read_query_test(&mut s),
-        //Some(mut s) => net::write_query_test(&mut s),
-        None => println!("Never established handshake... exiting...")
-    }
+    // let stream = net::connect();
+    // match stream {
+    //     Some(mut s) => net::read_query_test(&mut s),
+    //     //Some(mut s) => net::write_query_test(&mut s),
+    //     None => println!("Never established handshake... exiting...")
+    // }
+
+    let g = Term;
+    
 }
