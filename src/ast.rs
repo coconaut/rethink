4fd8bcd30930ab9e97966825d28b46830b560a0a
ambarry <ambarry22@gmail.com>
2015-11-29 16:58:02 -0500

    restructured for recursive terms

--- src/ast.rs
+++ src/ast.rs
@@ -5,6 +5,7 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::str;
 use std::mem;
+use std::boxed;
 use self::byteorder::{WriteBytesExt, LittleEndian, BigEndian};
 use net::*;
 
@@ -15,23 +16,23 @@ use net::*;
 //     _DB(DB)
 // }
 
-pub struct Term {
-    db: DB,
-    tt: TermType,
-    args: &'static str,
-    //optargs: &'static str,
-    // TODO: and need to deal with various compose overloads -> may need that enum after all...?
-    //chain: Vec<Term>
-}
 
-impl Term {
-    pub fn run(self) {
+// -------------------------------------------
+// RqlQuery: this is the base trait for all query terms
+// -------------------------------------------
+pub trait RqlQuery {
+    // each term struct must have way to overload build method for the RqlQuery syntax
+    fn build(&self) -> String;
+
+    // standard run function: accepts a connection -> can have specifics that use defaults
+    fn run(&self, c: Connection) {
         let start = QueryType::START as u32;
         let token: u64 = 1;
 
-        let composed = self.compose();
+        let composed = self.build();
         //[1,[15, [[14, [\"DeppFans\"]], \"Animals\"]],{}]
         let wrapped = format!("[{0}, {1}, {{}}]", start, composed);
+        println!("about to send: {0}", wrapped);
         let bytes = wrapped.into_bytes();
 
         // take a slice
@@ -39,7 +40,7 @@ impl Term {
         let len = q.len();
         let mut buffer = [0; 100];
 
-        let ref mut stream = self.db.r.connection.stream.unwrap();
+        let ref mut stream = c.stream.unwrap();
 
         let _ = stream.write_u64::<BigEndian>(token);
         let _ = stream.write_u32::<LittleEndian>(len as u32);
@@ -48,25 +49,82 @@ impl Term {
         let msg = str::from_utf8(&mut buffer).unwrap();
         println!("{}", msg);
     }
+}
 
-    fn compose(&self) -> String {
-        let db_expr = self.db.compose();
-        // need to go forward in chain: for loop and wrap
-        // for now, just using this term!
-        //[15, [[14, [\"DeppFans\"]], \"Animals\"]]
-        let expressed = format!("[{0}, [{1}, \"{2}\"]]", self.tt as u32, db_expr, self.args);
-        expressed
+// -------------------------------------------
+// Term structs -> don't worry about compose, just build
+// -------------------------------------------
+
+// basic Term
+pub struct Term<T: RqlQuery> {
+    tt: TermType,
+    args: &'static str,
+    //options: &'static str, keyword args, etc. may be better way than string!
+    prev: Option<Box<T>> // TODO: this will probably be generic! T: RqlQuery
+}
+
+// impl Term {
+//     fn run(self) {
+//         let start = QueryType::START as u32;
+//         let token: u64 = 1;
+//
+//         let composed = self.build();
+//         //[1,[15, [[14, [\"DeppFans\"]], \"Animals\"]],{}]
+//         let wrapped = format!("[{0}, {1}, {{}}]", start, composed);
+//         let bytes = wrapped.into_bytes();
+//
+//         // take a slice
+//         let mut q = &bytes[..];
+//         let len = q.len();
+//         let mut buffer = [0; 100];
+//
+//         let ref mut stream = self.db.r.connection.stream.unwrap();
+//
+//         let _ = stream.write_u64::<BigEndian>(token);
+//         let _ = stream.write_u32::<LittleEndian>(len as u32);
+//         let _ = stream.write(q);
+//         let size = stream.read(&mut buffer).unwrap();
+//         let msg = str::from_utf8(&mut buffer).unwrap();
+//         println!("{}", msg);
+//         // may need to return self, combinator?
+//     }
+// }
+
+impl<T: RqlQuery> RqlQuery for Term<T> {
+    fn build(&self) -> String {
+        // TODO: make this more elegant
+        match self.prev {
+            None => {
+                //just need to build self
+                if self.args == "" {
+                    format!("[{0}]", self.tt as u32)
+                }
+                else {
+                    // TODO: JSON encode the args first!
+                    format!("[{0}, [\"{1}\"]]", self.tt as u32, self.args)
+                }
+            }
+            Some(ref prev_term) => {
+                // recursively build and wrap previous terms!
+                let pr = prev_term.build();
+                if self.args == "" {
+                    format!("[{0}, [{1}]]", self.tt as u32, pr)
+                }
+                else {
+                    format!("[{0}, [{1}, \"{2}\"]]", self.tt as u32, pr, self.args)
+                }
+            }
+        }
     }
 }
 
-// trait RqlQuery {
-//     // operators, specific qb functs
-//
-// }
-// impl on either term
 
+// -------------------------------------------
+// More specific RQL structs with defined functionality: db, table, etc.
+// -------------------------------------------
 
-// let r = R.new(stream);
+// the root struct. useful in holding defualts.
+// also contains db methods, connection methods, etc.
 pub struct R {
     pub connection: Connection
 }
@@ -86,15 +144,9 @@ impl R {
         let db = DB {r: self, tt: TermType::DB, name: db_name};
         db
     }
-
-    // TODO: send query through connection (incr token, send through stream)
-    // pub fn send(query: &[u8]) {
-    //
-    // }
-
-    //TODO: recv response (as string at this level? have connection -> stream read?)
 }
 
+// the DB struct -> has table methods
 pub struct DB {
     r: R,
     tt: TermType,
@@ -102,13 +154,48 @@ pub struct DB {
 }
 
 impl DB {
-    pub fn table(self, table_name: &'static str) -> Term {
-        let t = Term {db: self, tt: TermType::TABLE, args: table_name};
-        t
+    pub fn table(self, table_name: &'static str) -> Table {
+        Table::new(self, table_name)
     }
 
-    pub fn compose(&self) -> String {
-        let db_expr = format!("[{0}, [\"{1}\"]]", self.tt as u32, self.name);
-        db_expr
+    //table list
+    pub fn table_list(self) -> Term<DB> {
+        Term {tt: TermType::TABLE_LIST, args: "", prev: Some(Box::new(self))}
+    }
+
+    //table create
+
+
+    //table drop
+
+}
+
+impl RqlQuery for DB {
+    fn build(&self) -> String {
+        format!("[{0}, [\"{1}\"]]", self.tt as u32, self.name)
+    }
+}
+
+pub struct Table {
+    db: DB,
+    tt: TermType,
+    args: &'static str,
+}
+
+impl Table {
+    pub fn new(db: DB, args: &'static str) -> Table {
+        let t = Table {db: db, tt: TermType::TABLE, args: args};
+        t
+    }
+}
+
+impl RqlQuery for Table {
+    fn build(&self) -> String {
+        let db_expr = self.db.build();
+        // need to go forward in chain: for loop and wrap
+        // for now, just using this term!
+        //[15, [[14, [\"DeppFans\"]], \"Animals\"]]
+        let expressed = format!("[{0}, [{1}, \"{2}\"]]", self.tt as u32, db_expr, self.args);
+        expressed
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,11 @@ fn main() {
     // }
 
     let mut r = R::new();
-    r.connect();
-    r.db("DeppFans").table("Animals").run();
-
+    //r.connect();
+    // worry about r.use type deal later..
+    let mut conn = net::Connection::new();
+    conn.connect();
+    //r.db("DeppFans").table("Animals").run(conn);
+    r.db("DeppFans").table_list().run(conn);
 
 }
